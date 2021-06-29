b75250b01a86105e74d88be333e0b748ff7ecdb9
FaBLee <58670304+fablee1@users.noreply.github.com>
2021-06-29 15:10:38 +0100

    updated server js

--- src/server.js
+++ src/server.js
@@ -20,6 +20,8 @@ server.use(
     origin: (origin, callback) => {
       if (!origin || whitelist.indexOf(origin) !== -1) {
         callback(null, true)
+      } else {
+        callback(new Error("Not allowed by cors!"))
       }
     },
   })
