b674ce18fcbcbba35bc9f1d0ad42f147645e88d8
FaBLee <58670304+fablee1@users.noreply.github.com>
2021-06-30 09:11:49 +0100

    strange things

--- src/middlewares/sanitize/authors/authorsSanitize.js
+++
@@ -1,8 +0,0 @@
-import filterBody from "./../filterBody.js"
-
-const authorsFields = ["name", "surname", "email", "avatar", "dob"]
-
-export const filterAuthorsBody = (req, res, next) => {
-  req["body"] = filterBody(req["body"], authorsFields)
-  next()
-}
--- src/middlewares/sanitize/filterBody.js
+++
@@ -1,6 +0,0 @@
-import _ from "lodash"
-
-const filterBody = (obj, keys) => {
-  return _.pick(obj, keys)
-}
-export default filterBody
